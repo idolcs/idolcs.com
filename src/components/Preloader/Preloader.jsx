450a3d2c5115a1d322a1c5569af6d781e1b509c4
Yash Kolambekar <yashskolambekar2015@gmail.com>
2024-04-25 22:39:21 +0530

    added preloader transition and framer-motion library resolve  #9

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
 import Logo from "../../assets/logo/logo-white.svg";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Preloader = (props) => {
 
+
+  const [showPreloader2, setShowPreloader2] = useState(true);
+
+  const variants = {
+    open: { opacity: 1},
+    closed: { opacity: 0}
+  };
+
   const showPreloader = props.showPreloader;
   // const showPreloader = true;
+
+  useEffect(() => {
+    if(showPreloader == false){
+      setTimeout(() => {
+        setShowPreloader2(false);
+      }, 500);
+    }
+  }, [showPreloader])
 
   let showBalls = true;
 
@@ -13,8 +32,12 @@ const Preloader = (props) => {
 
   return (
     <>
-      <div
-        className={`fixed left-0 top-0 z-[10000000000000] ${showPreloader ? "flex" : "hidden"} h-[100dvh] w-[100%] flex-col items-center justify-center overflow-hidden transition-all duration-500`}
+      <motion.div
+        variants={variants}
+        initial="open"
+        animate={showPreloader ? "open" : "closed"}
+        transition={{ duration: 0.3 }}
+        className={`fixed left-0 top-0 z-[10000000000000] h-[100dvh] w-[100%] flex-col items-center justify-center overflow-hidden transition-all duration-500 ${showPreloader2 ? "flex" : "hidden"}`}
         style={{
           background: "radial-gradient(#333394 0%, #13135B 100%)",
         }}
@@ -27,7 +50,7 @@ const Preloader = (props) => {
 
         <div className={`${showBalls ? "" : "hidden"} animate-loader-balls absolute bottom-[-10%] left-[-30%] aspect-square h-[45%] rounded-full bg-gradient-to-tr from-[#032E81] via-[#032E81] to-[#8BB3FF] sm:bottom-[-10%] sm:left-[-8%] sm:h-[70%]`}></div>
         <div className={`${showBalls ? "" : "hidden"} animate-loader-balls absolute right-[-30%] top-[-10%] aspect-square h-[34%] rounded-full bg-gradient-to-bl from-[#032E81] via-[#032E81] to-[#8BB3FF] sm:right-[-5%] sm:top-[-8%] sm:h-[80%]`}></div>
-      </div>
+      </motion.div>
     </>
   );
 };
