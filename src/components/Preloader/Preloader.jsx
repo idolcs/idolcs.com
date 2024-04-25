03c631160eb37c3efaf9c62c8cdb1d756d752f87
Yash Kolambekar <yashskolambekar2015@gmail.com>
2024-04-25 20:40:38 +0530

    Experimental Preloader

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,14 +1,8 @@
 import Logo from "../../assets/logo/logo-white.svg";
 
 const Preloader = (props) => {
-    
   const showPreloader = props.showPreloader;
-
-  let showBalls = true;
-
-  if (window.innerWidth > 640) {
-    showBalls = false;
-  }
+  // const showPreloader = true;
 
   return (
     <>
@@ -22,8 +16,14 @@ const Preloader = (props) => {
           <img className="animate-loader-logo pb-10" src={Logo} alt="" />
         </div>
 
-        <div className={`${showBalls ? "" : "hidden"} animate-loader-balls absolute bottom-[-10%] left-[-30%] aspect-square h-[45%] rounded-full bg-gradient-to-tr from-[#032E81] via-[#032E81] to-[#8BB3FF] sm:bottom-[-10%] sm:left-[-8%] sm:h-[70%]`}></div>
-        <div className={`${showBalls ? "" : "hidden"} animate-loader-balls absolute right-[-30%] top-[-10%] aspect-square h-[34%] rounded-full bg-gradient-to-bl from-[#032E81] via-[#032E81] to-[#8BB3FF] sm:right-[-5%] sm:top-[-8%] sm:h-[80%]`}></div>
+        <div className="animate-loader-dots dotted-background fixed left-0 top-0 h-[100dvh] w-full"></div>
+
+        {/* <div
+          className={`${showBalls ? "" : "hidden"} animate-loader-balls absolute bottom-[-10%] left-[-30%] aspect-square h-[45%] rounded-full bg-gradient-to-tr from-[#032E81] via-[#032E81] to-[#8BB3FF] sm:bottom-[-10%] sm:left-[-8%] sm:h-[70%]`}
+        ></div>
+        <div
+          className={`${showBalls ? "" : "hidden"} animate-loader-balls absolute right-[-30%] top-[-10%] aspect-square h-[34%] rounded-full bg-gradient-to-bl from-[#032E81] via-[#032E81] to-[#8BB3FF] sm:right-[-5%] sm:top-[-8%] sm:h-[80%]`}
+        ></div> */}
       </div>
     </>
   );
