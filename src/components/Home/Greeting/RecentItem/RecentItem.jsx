0f615f83e4eb2620c7b56d4cb79b2c2faa93fbfa
Yash Kolambekar <yashskolambekar2015@gmail.com>
2024-04-28 02:33:47 +0530

    circle url updated

--- src/components/Home/Greeting/RecentItem/RecentItem.jsx
+++ src/components/Home/Greeting/RecentItem/RecentItem.jsx
@@ -3,7 +3,7 @@ import Circle from "../../../../assets/images/inner-circle.svg";
 const RecentItem = (props) => {
   return (
     <>
-      <div style={{backgroundImage: `url(${Circle}`}} className="p-3 min-w-[166px] bg-[#787CF8] rounded-md mr-2 sm:mr-4 sm:pt-4 flex flex-col justify-between bg-blend-overlay bg-no-repeat bg-right-bottom">
+      <div className={`p-3 min-w-[166px] bg-[#787CF8]  bg-[url(${Circle})] rounded-md mr-2 sm:mr-4 sm:pt-4 flex flex-col justify-between bg-blend-overlay bg-no-repeat bg-right-bottom`}>
         <h3 className="text-base font-semibold sm:font-medium">{props["item"]["name"]}</h3>
         <div className="mt-1 sm:mt-2 pt-4">
           <p className="h-[1.3em] text-xs opacity-70">
